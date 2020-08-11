03466ad2bd3ea99f66e46c55101321d51ec0a66a
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-11 17:40:22 +0530

    Part Time Added

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,12 +2,18 @@
 echo "Welcome to Employee Wage Computation Program"
 WagePerHr=20
 FullHr=8
-check=$(( RANDOM%2 ))
+PartHr=4
+check=$(( RANDOM%3 ))
 if [[ $check -eq 1 ]]
 then
-	echo "Employee Present"
+	echo "Full Time Employee Present"
 	salary=$(( $WagePerHr * $FullHr ))
 	echo "Salary: "$salary
+elif [[ $check -eq 2 ]]
+then
+	echo "Part Time Employee Present"
+	salary=$(( $WagePerHr * $PartHr ))
+	echo "Salary: " $salary
 else
 	echo "Employee Absent"
 	echo "Salary: 0"
