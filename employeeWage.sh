ed14093f7cb6654df6f11589822825aee46c42cd
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-11 14:14:30 +0530

    Daily Wage Added

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
+WagePerHr=20
+FullHr=8
 check=$(( RANDOM%2 ))
 if [[ $check -eq 1 ]]
 then
 	echo "Employee Present"
+	salary=$(( $WagePerHr * $FullHr ))
+	echo "Salary: "$salary
 else
 	echo "Employee Absent"
+	echo "Salary: 0"
 fi
