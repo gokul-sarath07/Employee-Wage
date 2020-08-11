5b4a20bced0f188f64b631d17c52732d1edfb15f
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-11 17:57:34 +0530

    Monthly Salary Added

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,20 +3,21 @@ echo "Welcome to Employee Wage Computation Program"
 WagePerHr=20
 FullHr=8
 PartHr=4
+DaysInMonth=20
 check=$(( RANDOM%3 ))
 case $check in
 	1)
 		echo "Full Time Employee Present"
-		salary=$(( $WagePerHr * $FullHr ))
-		echo "Salary: "$salary
+		salary=$(( $WagePerHr * $FullHr * $DaysInMonth))
+		echo "Monthly Salary: "$salary
 		;;
 	2)
 		echo "Part Time Employee Present"
-		salary=$(( $WagePerHr * $PartHr ))
-		echo "Salary: " $salary
+		salary=$(( $WagePerHr * $PartHr * $DaysInMonth))
+		echo "Monthly Salary: " $salary
 		;;
 	0)
 		echo "Employee Absent"
-		echo "Salary: 0"
+		echo "Monthly Salary: 0"
 		;;
 esac
