83050083e78290d7e874c7ab7c54e9d0a6386c17
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-11 18:28:08 +0530

    Conditions Added

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Program"
-WagePerHr=20
-FullHr=8
-PartHr=4
-DaysInMonth=20
-check=$(( RANDOM%3 ))
-case $check in
-	1)
-		echo "Full Time Employee Present"
-		salary=$(( $WagePerHr * $FullHr * $DaysInMonth))
-		echo "Monthly Salary: "$salary
-		;;
-	2)
-		echo "Part Time Employee Present"
-		salary=$(( $WagePerHr * $PartHr * $DaysInMonth))
-		echo "Monthly Salary: " $salary
-		;;
-	0)
-		echo "Employee Absent"
-		echo "Monthly Salary: 0"
-		;;
-esac
+Wage_Per_Hr=20
+Days_In_Month=20
+Total_Working_Hrs=100
+Full_Time=1
+Part_Time=2
+
+Total_Emp_Hr=0
+Total_Working_Days=0
+while [[ $Total_Emp_Hr -lt $Total_Working_Hrs && $Total_Working_Days -lt $Days_In_Month ]]
+do
+	(( Total_Working_Days++ ))
+	check=$(( RANDOM%3 ))
+	case $check in
+		$Full_Time)
+			empHr=8
+			;;
+		$Part_Time)
+			empHr=4
+			;;
+
+		*)
+			empHr=0
+			;;
+	esac
+	Total_Emp_Hr=$(( $Total_Emp_Hr + $empHr ))
+done
+totalSalary=$(($Total_Emp_Hr * $Wage_Per_Hr))
