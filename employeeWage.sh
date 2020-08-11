c4cbb01f830c4961e54ecb54510f3cc6f73dd5a5
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-11 19:09:38 +0530

    Code Refactored

--- employeeWage.sh
+++ employeeWage.sh
@@ -8,22 +8,22 @@ Part_Time=2
 
 Total_Emp_Hr=0
 Total_Working_Days=0
-while [[ $Total_Emp_Hr -lt $Total_Working_Hrs && $Total_Working_Days -lt $Days_In_Month ]]
-do
-	(( Total_Working_Days++ ))
-	check=$(( RANDOM%3 ))
-	case $check in
+function getWorkingHrs {
+	case $1 in
 		$Full_Time)
 			empHr=8
 			;;
 		$Part_Time)
 			empHr=4
 			;;
-
 		*)
 			empHr=0
 			;;
 	esac
+}
+while [[ $Total_Emp_Hr -lt $Total_Working_Hrs && $Total_Working_Days -lt $Days_In_Month ]]
+do
+	(( Total_Working_Days++ ))
+	getWorkingHrs $(( RANDOM%3 ))
 	Total_Emp_Hr=$(( $Total_Emp_Hr + $empHr ))
 done
-totalSalary=$(($Total_Emp_Hr * $Wage_Per_Hr))
